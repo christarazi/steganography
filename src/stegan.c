1b4f6f8f83115ad4efcb7875c6972b7a4eb5decb
Chris Tarazi <tarazichris@gmail.com>
2017-03-26 20:35:04 -0700

    Implement LSB method for files

--- src/stegan.c
+++ src/stegan.c
@@ -7,7 +7,9 @@ static void hide_msg_lsb(struct BMP_file * const bmp, char const *msg,
 static void reveal_msg(struct BMP_file * const bmp);
 static void reveal_msg_lsb(struct BMP_file * const bmp);
 static void hide_file(struct BMP_file * const bmp, char const *hfile);
+static void hide_file_lsb(struct BMP_file * const bmp, char const *hfile);
 static void reveal_file(struct BMP_file * const bmp);
+static void reveal_file_lsb(struct BMP_file * const bmp);
 
 /*
  * This function is the public interface which invokes the appropriate
@@ -277,6 +279,78 @@ static void hide_file(struct BMP_file * const bmp, char const *hfile)
 }
 
 /*
+ * Hides a file by the name of |hfile| inside image using LSB method.
+ *
+ * The file is hidden in the blue channel of the RGB pixel.
+ */
+static void hide_file_lsb(struct BMP_file * const bmp, char const *hfile)
+{
+	/* Number of blue bytes - 32 bytes to store size of file */
+	size_t maxlimit = (bmp->datalen / 3) - (8 * 4);
+
+	FILE *hfp = fopen(hfile, "rb");
+	if (!hfp) {
+		perror("fopen");
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	size_t hidelen;
+	if (!get_file_size(hfp, &hidelen)) {
+		fprintf(stderr, "Error: could not obtain size of file\n");
+		fclose(hfp);
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	if (hidelen > maxlimit) {
+		fprintf(stderr, "Error: file too large to hide inside image\n");
+		fclose(hfp);
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	unsigned char *hdata = read_file(hfp, hidelen);
+	if (!hdata) {
+		fprintf(stderr, "Error: could not read file\n");
+		fclose(hfp);
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	unsigned char bit;
+	unsigned char data;
+	size_t h = 0;
+	size_t d = 0;
+
+	/* Write size of file in the first 32 blue bytes */
+	for (int8_t j = 0; j < 32; j++) {
+		bit = (hidelen >> j) & 1;
+		data = bmp->data[d].b;
+
+		/* Change 0th bit (LSB) to |bit| */
+		data = (data & ~(1 << 0)) | (bit << 0);
+
+		bmp->data[d].b = data;
+		d++;
+	}
+
+	while (h < hidelen && d < maxlimit) {
+		for (int8_t j = 0; j < 8; j++) {
+			bit = (hdata[h] >> j) & 1;
+			data = bmp->data[d].b;
+
+			/* Change 0th bit (LSB) to |bit| */
+			data = (data & ~(1 << 0)) | (bit << 0);
+
+			bmp->data[d].b = data;
+			d++;
+		}
+
+		h++;
+	}
+
+	free(hdata);
+	fclose(hfp);
+}
+
+/*
  * Reveals file hidden within image.
  *
  * This function does the opposite of hide_file(), but does not alter the
@@ -310,6 +384,77 @@ static void reveal_file(struct BMP_file * const bmp)
 	 */
 	for (size_t i = 0; i < hidelen; i++)
 		hdata[i] = bmp->data[i + 4].b;
+
+	char outname[] = "outXXXXXX";
+	int outfd = mkstemp(outname);
+	if (outfd < 0) {
+		perror("mkstemp");
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	if (write(outfd, hdata, hidelen) < 0) {
+		perror("write");
+		close(outfd);
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	close(outfd);
+	printf("Decoded file: %s\n", outname);
+}
+
+/*
+ * Reveals file hidden using the LSB method within image.
+ *
+ * This function does the opposite of hide_file_lsb(), but does not alter the
+ * |data| values; just creates the revealed file.
+ */
+static void reveal_file_lsb(struct BMP_file * const bmp)
+{
+	size_t hidelen = 0; /* Size of hidden file */
+
+	/* Size of file is stored in the first 32 blue bytes */
+	unsigned char buf[32] = { 0 };
+	for (int8_t i = 0; i < 32; ++i) {
+		buf[i] = (bmp->data[i].b >> 0) & 1;
+		hidelen += ((1 << i) * buf[i]);
+	}
+
+	/* Prevent out-of-bounds access to |bmp->data| */
+	size_t fullsize = (hidelen + 4) * 8;
+	size_t maxlimit = (bmp->datalen / 3);
+	if (fullsize > maxlimit) {
+		fprintf(stderr,
+			"Error: length mismatch found; possibly corrupt\n");
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	unsigned char *hdata = malloc(hidelen);
+	if (!hdata) {
+		perror("malloc");
+		clean_exit(bmp->fp, bmp->data, EXIT_FAILURE);
+	}
+
+	/*
+	 * Begin extracting the hidden file data. Start at offset of 32 because the
+	 * first 32 bytes contain the size of the file.
+	 */
+	size_t h = 0;                 /* Index for |hdata| */
+	size_t datalen = hidelen * 8; /* Bytes in |bmp->data| */
+	unsigned char count = 0;
+	unsigned char data = 0;
+	for (size_t i = 0; i < datalen; ++i) {
+		buf[i % 8] = (bmp->data[i + 32].b >> 0) & 1;
+		count++;
+
+		if (count == 8) {
+			count = 0;
+			data = 0;
+			for (int8_t j = 7; j >= 0; --j)
+				data += (buf[j] << j);
+
+			hdata[h++] = data;
+		}
+	}
 
 	char outname[] = "outXXXXXX";
 	int outfd = mkstemp(outname);
